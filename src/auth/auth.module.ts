15a25c606f66221021e1e9f9821523c15a5c4209
Andifadly12 <andifadly2004@gmail.com>
2026-08-24 19:24:36 +0800

    commit 2

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  imports: [PrismaService],
+  imports: [PrismaModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
