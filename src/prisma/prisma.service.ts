296ab6ede50cb5a4419821ff9ebc2812c3d8cb5b
Andifadly12 <andifadly2004@gmail.com>
2026-08-24 19:57:16 +0800

    perbaikan error

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,22 @@
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not configured');
+    }
+
+    super({
+      adapter: new PrismaPg({ connectionString }),
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
