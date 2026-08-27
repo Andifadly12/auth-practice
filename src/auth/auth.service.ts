c201d9dcaf3d4d83f7e1b0bc9d7c992e7f850eac
Andifadly12 <andifadly2004@gmail.com>
2026-08-27 22:34:04 +0800

    buat prisama dan register di services

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(dto: RegisterDto) {
+    const existingEmail = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (existingEmail) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    const existingUsername = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (existingUsername) {
+      throw new BadRequestException('Username already exists');
+    }
+
+    const passwordHash = await bcrypt.hash(dto.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        username: dto.username,
+        umur: dto.umur,
+        email: dto.email,
+        name: dto.username,
+        passwordHash,
+      },
+      select: {
+        id: true,
+        username: true,
+        umur: true,
+        name: true,
+        email: true,
+        createdAt: true,
+      },
+    });
+
+    return {
+      message: 'Register berhasil',
+      user,
+    };
+  }
+}
