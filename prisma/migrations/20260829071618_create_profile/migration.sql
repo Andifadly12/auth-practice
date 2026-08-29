89a6ef48a69687ecf8b5d48f6e90be34019a688b
Andifadly12 <andifadly2004@gmail.com>
2026-08-29 15:17:01 +0800

    buat prfile

---
+++ prisma/migrations/20260829071618_create_profile/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "profiles" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "bio" TEXT,
+    "phone" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profiles_user_id_key" ON "profiles"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
