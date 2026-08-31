4626abde1dbe34f949f0f45d2c544328b23b9ec7
Andifadly12 <andifadly2004@gmail.com>
2026-08-31 20:52:50 +0800

    dto

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getMyProfile(userId: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+
+      select: {
+        id: true,
+        username: true,
+        umur: true,
+        email: true,
+        name: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+
+      profile: {
+        select: {
+          id: true,
+          bio: true,
+          phone: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('penggguna tidak aditemukan ');
+    }
+    return {
+      message: 'Profile hasil diambil',
+      profile: user,
+    };
+  }
+}
